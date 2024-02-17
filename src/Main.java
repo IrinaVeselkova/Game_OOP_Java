e2df5c82840c621ccc4942c24c25d738b02ee34d
Veselkova Irina <veselkovaiv@yandex.ru>
2024-02-17 19:39:15 +0300

    Change distance method

--- src/Main.java
+++ src/Main.java
@@ -9,41 +9,10 @@ import static java.lang.Math.sqrt;
 public class Main {
     public static void main(String[] args) {
 
-        List<Character> team1 = new ArrayList<>();
-        List<Character> team2 = new ArrayList<>();
+        List<Character> team1 = createTeam(10,0);
+        List<Character> team2 = createTeam(10,9);
 
-        for (int i = 0; i < 10; i++) {
-            switch (new Random().nextInt(1,8)) {
-                case 1:
-                    team1.add(new Witcher(Character.getName(), i, 0));
-                    team2.add(new Witcher(Character.getName(), i, 9));
-                    break;
-                case 2:
-                    team1.add(new Bandit(Character.getName(), i, 0));
-                    team2.add(new Bandit(Character.getName(), i, 9));
-                    break;
-                case 3:
-                    team1.add(new Crossbowman(Character.getName(), i, 0));
-                    team2.add(new Crossbowman(Character.getName(), i, 9));
-                    break;
-                case 4:
-                    team1.add(new Peasant(Character.getName(), i, 0));
-                    team2.add(new Peasant(Character.getName(), i, 9));
-                    break;
-                case 5:
-                    team1.add(new Sniper(Character.getName(), i, 0));
-                    team2.add(new Sniper(Character.getName(), i, 9));
-                    break;
-                case 6:
-                    team1.add(new Spear(Character.getName(), i, 0));
-                    team2.add(new Spear(Character.getName(), i, 9));
-                    break;
-                case 7:
-                    team1.add(new Monk(Character.getName(), i, 0));
-                    team2.add(new Monk(Character.getName(), i, 9));
-                    break;
-            }
-        }
+//
 
         for (Character character: team1) {
             System.out.println("team1");
@@ -55,8 +24,44 @@ public class Main {
             System.out.println(character.toString());
             System.out.println();
         }
-
-        System.out.println("Ближайший к\n" + team1.get(1) + "\nпротивник: =>\n" + team1.get(1).getMinDistance(team2));
+// ищем в команде лучника (crossbowman) и находим к нему ближайшего противника
+        for (Character cbm : team1) {
+            if (cbm.getClass().getSimpleName().equals("Crossbowman")||cbm.getClass().getSimpleName().equals("Sniper")) {
+                System.out.println("Ближайший к\n" + cbm + "\nпротивник: =>\n" + cbm.getMinDistance(team2));
+                System.out.println();
+            }
+        }
 
     }
+
+    static List<Character> createTeam(int quantityHeroes, int y) {
+        List<Character> team = new ArrayList<>();
+        for (int i = 0; i < quantityHeroes; i++) {
+            switch (new Random().nextInt(1, 8)) {
+                case 1:
+                    team.add(new Witcher(Character.getName(), i, y));
+                    break;
+                case 2:
+                    team.add(new Bandit(Character.getName(), i, y));
+                    break;
+                case 3:
+                    team.add(new Crossbowman(Character.getName(), i, y));
+                    break;
+                case 4:
+                    team.add(new Peasant(Character.getName(), i, y));
+                    break;
+                case 5:
+                    team.add(new Sniper(Character.getName(), i, y));
+                    break;
+                case 6:
+                    team.add(new Spear(Character.getName(), i, y));
+                    break;
+                case 7:
+                    team.add(new Monk(Character.getName(), i, y));
+                    break;
+            }
+        }
+    return team;
+    }
+
 }
